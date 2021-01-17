3db2e926b06f746aa8d8bf31689be561a962be47
kevinsnielsen <forgeofatlas@gmail.com>
2021-01-16 17:50:03 -0800

    W2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 CSE 341 Web Backend Development II
+
+git status
+
+git add [file]
+
+git push heroku master
+git push github master
+
+heroku open
--- web/config.php
+++ web/config.php
@@ -1,14 +1,13 @@
 <?php
-// Check for local build
+// Setting for Local and live builds
 if(preg_match("/C:\/xampp\/htdocs/", $_SERVER["DOCUMENT_ROOT"])) {
 
 	// Root adjustment
-	$_SERVER['DOCUMENT_ROOT'] .= '/cse341/web/';
+	$_SERVER['DOCUMENT_ROOT'] .= '/cse341/web';
 
 } else {
-	// No setting at this timeRoot 
+	// No setting at this timeRoot
 }
 
 // Site Time
 date_default_timezone_set('America/Los_Angeles');
-echo $_SERVER['DOCUMENT_ROOT'];
