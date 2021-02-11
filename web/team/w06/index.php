645a8fa627ac564742a25992b407f6eece21a165
kevinsnielsen <forgeofatlas@gmail.com>
2021-02-10 18:48:56 -0800

    team

--- web/team/w06/index.php
+++ web/team/w06/index.php
@@ -1,42 +1,49 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'] . '/web/library/connections.php';
+require 'web/library/connections.php';
 $db;
 
 $topics = $db->query('SELECT id, name FROM team06.topic', PDO::FETCH_ASSOC);
 
+$stmt = $db->prepare('SELECT id, book, chapter, verse, content FROM team06.scriptures WHERE id=:id');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->execute();
+$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
 
-if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
-	
-	$id = filter_input(INPUT_GET, 'id', FILTER_INT FILTER_SANITIZE_NUMBER_INT);
+$stmt = $db->prepare('SELECT t.name 
+FROM team06.scripture_topic s 
+JOIN team06.topic t 
+IN s.topic_id = t.id WHERE scripture_id=:id');
 
-	$stmt = $db->prepare('SELECT id, book, chapter, verse, content FROM team06.scriptures WHERE id=:id');
-	$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$scripture_list =[];
+foreach($scriptures as $scripture) {
+	$scripture_list = $scripture;
+	$stmt->bindValue(':id', $scripture['id'], PDO::PARAM_INT);
 	$stmt->execute();
-	$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	$stmt->closeCursor();
-
-	$stmt = $db->prepare('SELECT t.name 
-	FROM team06.scripture_topic s 
-	JOIN team06.topic t 
-	IN s.topic_id = t.id WHERE scripture_id=:id');
-	
-	$scripture_list =[];
-	foreach($scriptures as $scripture) {
-		$scripture_list = $scripture;
-		$stmt->bindValue(':id', $scripture['id'], PDO::PARAM_INT);
-		$stmt->execute();
-		$scripture_list['topics'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
-	$stmt->closeCursor();
+	$scripture_list['topics'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+$stmt->closeCursor();
 
 
-	print_r($scripture_list);
+print_r($scripture_list);
 
 
 	echo '<div><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - "' . $row['content'] . '</div>';
 
-}
+if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
+	
+	//$id = filter_input(INPUT_GET, 'id', FILTER_INT, FILTER_SANITIZE_NUMBER_INT);
 
+	$statement = $db -> prepare('INSERT INTO team06.scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)');
+	$statement -> execute(['book' => $_POST['book'], 'chapter' => $_POST['chapter'], 'verse' => $_POST['verse'], 'content' => $_POST['content']]);
+	$scriptureID = $db -> lastInsertId("scripture_id_seq");
+
+	foreach ($_POST['topics'] as $topicID) {
+		echo "Topic: $topicID, Scripture: $scriptureID <br>";
+		$statement = $db -> prepare('INSERT INTO team06.scripture_topic (topicID, scriptureID) VALUES (?, ?)');
+		$statement -> execute([$topicID, $scriptureID]);
+	}
+}
 ?>
 
 <form method="POST">
@@ -47,7 +54,7 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
 
 	Topic <br>
 	<?php foreach($topics as $topic) { ?>
-		<label> <input type="checkbox" name="topic[]" value="<?=$topic['id']?>"> <?=$topic['name']?></label>
+		<label> <input type="checkbox" name="topics[]" value="<?=$topic['id']?>"> <?=$topic['name']?></label>
 	<?php } ?>
 	<input type='submit' value='submit'>
 </from>
