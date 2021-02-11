0c5e0923720fe596ee2770d0c1a97ffd160082fa
kevinsnielsen <forgeofatlas@gmail.com>
2021-02-10 19:00:58 -0800

    team

--- web/team/w06/index.php
+++ web/team/w06/index.php
@@ -2,33 +2,26 @@
 require 'connections.php';
 $db = dbConnect();
 
-$topics = $db->query('SELECT id, name FROM team06.topic', PDO::FETCH_ASSOC);
-
-$stmt = $db->prepare('SELECT id, book, chapter, verse, content FROM team06.scriptures WHERE id=:id');
-$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-$stmt->execute();
-$scriptures = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$stmt->closeCursor();
+$topics = $db->query('SELECT id, name FROM team06.topic'
+$scriptures =  $db->query('SELECT id, book, chapter, verse, content FROM team06.scriptures', PDO::FETCH_ASSOC);
 
 $stmt = $db->prepare('SELECT t.name 
-FROM team06.scripture_topic s 
-JOIN team06.topic t 
-IN s.topic_id = t.id WHERE scripture_id=:id');
+	FROM team06.scripture_topic s 
+	JOIN team06.topic t 
+	IN s.topic_id = t.id WHERE scripture_id=:id');
 
 $scripture_list =[];
 foreach($scriptures as $scripture) {
-	$scripture_list = $scripture;
 	$stmt->bindValue(':id', $scripture['id'], PDO::PARAM_INT);
 	$stmt->execute();
-	$scripture_list['topics'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$scripture['topics'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$scripture_list[] = $scripture;
 }
 $stmt->closeCursor();
 
 
 print_r($scripture_list);
-
-
-	echo '<div><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - "' . $row['content'] . '</div>';
+exit;
 
 if (($_SERVER['REQUEST_METHOD'] == 'POST')) {
 	
